5949a24a69c9d28f6a4975a2f7c81868853015e1
mohamedelsayed1280 <mohamed.elsayed12008@gmail.com>
2023-08-05 19:51:41 +0300

    add

---
+++ C programming/STD_TYPES.h
@@ -0,0 +1,10 @@
+
+
+typedef unsigned char      uint8 ; 
+typedef char               sint8 ; 
+typedef unsigned short int uint8 ;
+typedef short int         sint16 ; 
+typedef unsigned long int unit32 ; 
+typedef long int          snit32 ; 
+typedef float            float32 ; 
+typedef double           float64 ;
